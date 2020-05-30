b7c7fc497526684e6a1df7bd1d625ab85ebf208d
Anton <sidorevich.anton@gmail.com>
2020-05-30 03:59:25 +0300

    Implemented test functions and added test samples

--- src/test/java/com/plugin/engine/FileFormatterTest.java
+++ src/test/java/com/plugin/engine/FileFormatterTest.java
@@ -1,5 +1,35 @@
 package com.plugin.engine;
 
-public class FileFormatterTest {
+import com.google.googlejavaformat.java.FormatterException;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+public class FileFormatterTest extends FormatterTest {
+    private boolean testSamples(String dirPath) throws IOException, FormatterException {
+        for (File file : getFile(dirPath).listFiles()) {
+            if (file.getName().matches("\\d\\.java")) {
+                File copiedFile = copyFile(file, "file_" + getAnswerFile(file).getName());
+                new JavaFormatter(0).formatFile(copiedFile);
+                if (!FileUtils.contentEquals(copiedFile, getAnswerFile(file))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void testSmallSamples() throws IOException, FormatterException {
+        assertTrue(testSamples("java/small_samples"));
+    }
+
+    @Test
+    public void testBigSamples() throws IOException, FormatterException {
+        assertTrue(testSamples("java/big_samples"));
+    }
 }
--- src/test/java/com/plugin/engine/PartsFileFormatterTest.java
+++ src/test/java/com/plugin/engine/PartsFileFormatterTest.java
@@ -1,5 +1,42 @@
 package com.plugin.engine;
 
-public class PartsFileFormatterTest {
+import com.google.common.collect.Range;
+import com.google.googlejavaformat.java.FormatterException;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
+
+public class PartsFileFormatterTest extends FormatterTest {
+    private boolean testSample(FileChanges fileChanges) throws IOException, FormatterException {
+        File copiedFile = copyFile(fileChanges.getFile(), "parts_" + getAnswerFile(fileChanges.getFile()).getName());
+        new JavaFormatter(0).formatPartsOfFile(new FileChanges(copiedFile, fileChanges.getChanges()));
+        return FileUtils.contentEquals(copiedFile, getAnswerFile(fileChanges.getFile()));
+    }
+
+    @Test
+    public void testFirstSmallSample() throws IOException, FormatterException {
+        assertTrue(testSample(new FileChanges(
+                getFile("java/small_samples/1.java"),
+                new ArrayList<>() {{
+                    add(Range.open(6, 7));
+                    add(Range.open(171, 190));
+                }}
+        )));
+    }
+
+    @Test
+    public void testFirstBigSample() throws IOException, FormatterException {
+        assertTrue(testSample(new FileChanges(
+                getFile("java/big_samples/1.java"),
+                new ArrayList<>() {{
+                    add(Range.open(6, 7));
+                    add(Range.open(171, 190));
+                }}
+        )));
+    }
 }
