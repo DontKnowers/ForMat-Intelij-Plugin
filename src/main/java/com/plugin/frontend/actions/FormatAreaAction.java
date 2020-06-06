b394c7226886c8aebccd5809ec27174c9eeb1c22
ayfokin <ayfokin@mail.ru>
2020-06-06 22:05:56 +0500

    fixed bugs

--- src/main/java/com/plugin/frontend/actions/FormatAreaAction.java
+++ src/main/java/com/plugin/frontend/actions/FormatAreaAction.java
@@ -1,11 +1,11 @@
 package com.plugin.frontend.actions;
 
-import com.google.googlejavaformat.java.FormatterException;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -15,50 +15,40 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class FormatAreaAction extends AnAction {
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         JavaFormatter test = new JavaFormatter();
-        VirtualFile file = FileDocumentManager.getInstance().getFile(e.getData(LangDataKeys.EDITOR).getDocument());
+        VirtualFile file = FileDocumentManager.getInstance().getFile(Objects.requireNonNull(e.getData(LangDataKeys.EDITOR)).getDocument());
 
         Editor editor = e.getData(PlatformDataKeys.EDITOR);
 
-        int startOffset = editor.getSelectionModel().getSelectionStart();
-        int endOffset = editor.getSelectionModel().getSelectionEnd();
+        assert editor != null;
+        SelectionModel start = editor.getSelectionModel();
+        SelectionModel end = editor.getSelectionModel();
 
-        Integer startLine = 0;
-        Integer endLine = 0;
+        if (file == null) {
+            return;
+        }
+        int startOffset = start.getSelectionStart();
+        int endOffset = end.getSelectionEnd();
+        int startLine = 0;
+        int endLine = 0;
+
 
         try {
             LineNumberReader r = new LineNumberReader(new FileReader(new File(file.getPath())));
             int count = 0;
             int codeOfSymbol = 0;
             int crutch = 0;
-            while (count < startOffset && (codeOfSymbol = r.read()) != -1) {
-                count++;
-            }
-            if (count == startOffset) {
-                if (codeOfSymbol != 10) {
-                    crutch++;
-                }
-                startLine = r.getLineNumber() + crutch;
-            }
-
+            startLine = changeCount(startOffset, r, count, codeOfSymbol, crutch);
             crutch = 0;
-            while (count < endOffset && (codeOfSymbol = r.read()) != -1) {
-                count++;
-            }
-            if (count == endOffset) {
-                if (codeOfSymbol != 10) {
-                    crutch++;
-                }
-                endLine = r.getLineNumber() + crutch;
-            }
-        } catch (FileNotFoundException fileNotFoundException) {
-            fileNotFoundException.printStackTrace();
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
+            endLine = changeCount(endOffset, r, count, codeOfSymbol, crutch);
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
 
         Integer finalStartLine = startLine;
@@ -67,11 +57,23 @@ public class FormatAreaAction extends AnAction {
             test.formatPartsOfFile(new FileChanges(new File(file.getPath()), new ArrayList<>() {{
                 add(new Pair<>(finalStartLine, finalEndLine));
             }}));
-        } catch (FormatterException formatterException) {
-            formatterException.printStackTrace();
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
+    }
+
+    private int changeCount(int offset, LineNumberReader r, int count, int codeOfSymbol, int crutch) throws IOException {
+        int line = 0;
+        while (count < offset && (codeOfSymbol = r.read()) != -1) {
+            count++;
+        }
+        if (count == offset) {
+            if (codeOfSymbol != 10) {
+                crutch++;
+            }
+            line = r.getLineNumber() + crutch;
+        }
+        return line;
     }
 }
 
