a7548bb99304332c0edffb15b6a913e3df73afe6
ayfokin <ayfokin@mail.ru>
2020-06-09 01:43:29 +0500

    Fixed bugs #2 (vse rabotaet)

--- src/main/java/com/plugin/frontend/actions/FormatAreaAction.java
+++ src/main/java/com/plugin/frontend/actions/FormatAreaAction.java
@@ -1,11 +1,12 @@
 package com.plugin.frontend.actions;
 
+import com.google.googlejavaformat.java.FormatterException;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -15,60 +16,70 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class FormatAreaAction extends AnAction {
+    private int startOffset;
+    private int endOffset;
+    int count;
+    int codeOfSymbol;
+
+    LineNumberReader r;
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        JavaFormatter test = new JavaFormatter();
-        VirtualFile file = FileDocumentManager.getInstance().getFile(Objects.requireNonNull(e.getData(LangDataKeys.EDITOR)).getDocument());
-
-        Editor editor = e.getData(PlatformDataKeys.EDITOR);
-
-        assert editor != null;
-        SelectionModel start = editor.getSelectionModel();
-        SelectionModel end = editor.getSelectionModel();
+        JavaFormatter javaFormatter = new JavaFormatter();
+        VirtualFile file = null;
+        try {
+            Document document = (e.getData(LangDataKeys.EDITOR)).getDocument();
+            file = FileDocumentManager.getInstance().getFile(document);
+        } catch (NullPointerException exception) {
+            exception.printStackTrace();
+        }
 
         if (file == null) {
             return;
         }
-        int startOffset = start.getSelectionStart();
-        int endOffset = end.getSelectionEnd();
+
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        try {
+            startOffset = editor.getSelectionModel().getSelectionStart();
+            endOffset = editor.getSelectionModel().getSelectionEnd();
+        } catch (NullPointerException exception) {
+            exception.printStackTrace();
+        }
+
         int startLine = 0;
         int endLine = 0;
-
+        count = 0;
 
         try {
-            LineNumberReader r = new LineNumberReader(new FileReader(new File(file.getPath())));
-            int count = 0;
-            int codeOfSymbol = 0;
-            int crutch = 0;
-            startLine = changeCount(startOffset, r, count, codeOfSymbol, crutch);
-            crutch = 0;
-            endLine = changeCount(endOffset, r, count, codeOfSymbol, crutch);
-        } catch (Exception exception) {
+            r = new LineNumberReader(new FileReader(new File(file.getPath())));
+            startLine = changeCount(startOffset);
+            endLine = changeCount(endOffset);
+        } catch (IOException exception) {
             exception.printStackTrace();
         }
 
         Integer finalStartLine = startLine;
         Integer finalEndLine = endLine;
+
         try {
-            test.formatPartsOfFile(new FileChanges(new File(file.getPath()), new ArrayList<>() {{
+            javaFormatter.formatPartsOfFile(new FileChanges(new File(file.getPath()), new ArrayList<>() {{
                 add(new Pair<>(finalStartLine, finalEndLine));
             }}));
-        } catch (Exception exception) {
+        } catch (FormatterException | IOException exception) {
             exception.printStackTrace();
         }
     }
 
-    private int changeCount(int offset, LineNumberReader r, int count, int codeOfSymbol, int crutch) throws IOException {
+    private int changeCount(int offset) throws IOException {
         int line = 0;
+        int crutch = 0;
         while (count < offset && (codeOfSymbol = r.read()) != -1) {
             count++;
         }
         if (count == offset) {
-            if (codeOfSymbol != 10) {
+            if (codeOfSymbol != 10 || offset == startOffset) {
                 crutch++;
             }
             line = r.getLineNumber() + crutch;
--- src/main/java/com/plugin/frontend/actions/FormatFileAction.java
+++ src/main/java/com/plugin/frontend/actions/FormatFileAction.java
@@ -1,29 +1,37 @@
 package com.plugin.frontend.actions;
 
+import com.google.googlejavaformat.java.FormatterException;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.plugin.engine.JavaFormatter;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.util.Objects;
+import java.io.IOException;
 
 public class FormatFileAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        JavaFormatter test = new JavaFormatter();
-        VirtualFile file = FileDocumentManager.getInstance().getFile(Objects.requireNonNull(e.getData(LangDataKeys.EDITOR)).getDocument());
+        JavaFormatter javaFormatter = new JavaFormatter();
+        VirtualFile file = null;
+        try {
+            Document document = (e.getData(LangDataKeys.EDITOR)).getDocument();
+            file = FileDocumentManager.getInstance().getFile(document);
+        } catch (NullPointerException exception) {
+            exception.printStackTrace();
+        }
 
         if (file == null) {
             return;
         }
 
         try {
-            test.formatFile(new File(file.getPath()));
-        } catch (Exception exception) {
+            javaFormatter.formatFile(new File(file.getPath()));
+        } catch (FormatterException | IOException exception) {
             exception.printStackTrace();
         }
     }
