8a9eda746dfa01b76a21eabd5f2562ae1ad0ad6c
Руслан <ruslan.nasibullin.00@gmail.com>
2020-06-14 23:04:57 +0600

    Implemented Git Diff

--- src/main/java/com/plugin/frontend/actions/FormatDiffAction.java
+++ src/main/java/com/plugin/frontend/actions/FormatDiffAction.java
@@ -1,11 +1,35 @@
 package com.plugin.frontend.actions;
 
+import com.google.googlejavaformat.java.FormatterException;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.plugin.engine.FormatGitDiff;
+import com.plugin.engine.JavaFormatter;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class FormatDiffAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
+        JavaFormatter javaFormatter = new JavaFormatter();
+
+        try {
+            FormatGitDiff gitDiff = new FormatGitDiff();
+
+            String projDir = e.getData(LangDataKeys.EXECUTION_ENVIRONMENT).getProject().getBasePath();
+
+            ArrayList<File> gitDiffFiles = gitDiff.executeCommand(projDir);
+
+            for (File file : gitDiffFiles) {
+                javaFormatter.formatFile(file);
+            }
+
+        } catch (FormatterException | IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
